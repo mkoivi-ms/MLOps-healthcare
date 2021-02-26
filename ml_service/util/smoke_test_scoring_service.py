890ea9e20e99fe4f88908e6dd3b645b42768fd6f
Marko Koivisto <makoivis@microsoft.com>
2021-02-26 07:55:44 +0200

    initial commit

---
+++ ml_service/util/smoke_test_scoring_service.py
@@ -0,0 +1,98 @@
+import argparse
+import requests
+import time
+from azureml.core import Workspace
+from azureml.core.webservice import AksWebservice, AciWebservice
+from ml_service.util.env_variables import Env
+import secrets
+
+
+input = {"data": [[0, 1, 8, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 12, 1, 0,
+                   0, 0.5, 0.3, 0.610327781, 7, 1, -1, 0, -1, 1, 1, 1, 2, 1,
+                   65, 1, 0.316227766, 0.669556409, 0.352136337, 3.464101615,
+                   0.1, 0.8, 0.6, 1, 1, 6, 3, 6, 2, 9, 1, 1, 1, 12, 0, 1, 1, 0, 0, 1],
+                  [4, 2, 5, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 5, 1, 0, 0, 0.9,
+                   0.5, 0.771362431, 4, 1, -1, 0, 0, 11, 1, 1, 0, 1, 103, 1,
+                   0.316227766, 0.60632002, 0.358329457, 2.828427125, 0.4, 0.5,
+                    0.4, 3, 3, 8, 4, 10, 2, 7, 2, 0, 3, 10, 0, 0, 1, 1, 0, 1]]}
+
+output_len = 2
+
+
+def call_web_service(e, service_type, service_name):
+    aml_workspace = Workspace.get(
+        name=e.workspace_name,
+        subscription_id=e.subscription_id,
+        resource_group=e.resource_group
+    )
+    print("Fetching service")
+    headers = {}
+    if service_type == "ACI":
+        service = AciWebservice(aml_workspace, service_name)
+    else:
+        service = AksWebservice(aml_workspace, service_name)
+    if service.auth_enabled:
+        service_keys = service.get_keys()
+        headers['Authorization'] = 'Bearer ' + service_keys[0]
+    print("Testing service")
+    print(". url: %s" % service.scoring_uri)
+    output = call_web_app(service.scoring_uri, headers)
+
+    return output
+
+
+def call_web_app(url, headers):
+
+    # Generate an HTTP 'traceparent' distributed tracing header
+    # (per the W3C Trace Context proposed specification).
+    headers['traceparent'] = "00-{0}-{1}-00".format(
+        secrets.token_hex(16), secrets.token_hex(8))
+
+    retries = 600
+    for i in range(retries):
+        try:
+            response = requests.post(
+                url, json=input, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as e:
+            if i == retries - 1:
+                raise e
+            print(e)
+            print("Retrying...")
+            time.sleep(1)
+
+
+def main():
+
+    parser = argparse.ArgumentParser("smoke_test_scoring_service.py")
+
+    parser.add_argument(
+        "--type",
+        type=str,
+        choices=["AKS", "ACI", "Webapp"],
+        required=True,
+        help="type of service"
+    )
+    parser.add_argument(
+        "--service",
+        type=str,
+        required=True,
+        help="Name of the image to test"
+    )
+    args = parser.parse_args()
+
+    e = Env()
+    if args.type == "Webapp":
+        output = call_web_app(args.service, {})
+    else:
+        output = call_web_service(e, args.type, args.service)
+    print("Verifying service output")
+
+    assert "result" in output
+    assert len(output["result"]) == output_len
+    print("Smoke test successful.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ training-script/training/train.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import lightgbm
+
+
+def split_data(data_df):
+    """Split a dataframe into training and validation datasets"""
+
+    features = data_df.drop(['target', 'id'], axis=1)
+    labels = np.array(data_df['target'])
+    features_train, features_valid, labels_train, labels_valid = \
+        train_test_split(features, labels, test_size=0.2, random_state=0)
+
+    train_data = lightgbm.Dataset(features_train, label=labels_train)
+    valid_data = lightgbm.Dataset(
+        features_valid,
+        label=labels_valid,
+        free_raw_data=False
+        )
+
+    return (train_data, valid_data)
+
+
+def train_model(train_data, valid_data, parameters):
+    """Train a model with the given datasets and parameters"""
+    # The object returned by split_data is a tuple.
+    # Access train_data with data[0] and valid_data with data[1]
+
+    model = lightgbm.train(parameters,
+                           train_data,
+                           valid_sets=valid_data,
+                           num_boost_round=500,
+                           early_stopping_rounds=20)
+
+    return model
+
+
+#def get_model_metrics(model, train_data, valid_data):
+ #   """Construct a dictionary of metrics for the model"""
+#
+#    predictions = model.predict(valid_data.data)
+#    fpr, tpr, thresholds = metrics.roc_curve(valid_data.label, predictions)
+#    model_metrics = {"auc": (metrics.auc(fpr, tpr))}
+#
+#    return model_metrics
+
+def get_model_metrics(model, train_data, valid_data):
+    predictions = model.predict(valid_data.data)
+    fpr, tpr, thresholds = metrics.roc_curve(valid_data.label, predictions)
+    classified_perdection = predictions
+
+    for index in range(len(predictions)):
+        if(predictions[index] > 0.25):
+            classified_perdection[index] = 0
+        else: 
+            classified_perdection[index] = 1
+
+    #model_metrics = {"auc": (metrics.auc(fpr, tpr))}
+    model_metrics = {"auc": (metrics.auc(fpr, tpr)), "f1_score":(metrics.f1_score(valid_data.label, classified_perdection))}
+    #metrics.f1_score(valid_data.label, predictions)
+    return model_metrics
+
+
+    
+
+def main():
+    """This method invokes the training functions for development purposes"""
+
+    # Read data from a file
+    data_df = pd.read_csv('porto_seguro_safe_driver_prediction_input.csv')
+
+    # Hard code the parameters for training the model
+    parameters = {
+        'learning_rate': 0.02,
+        'boosting_type': 'gbdt',
+        'objective': 'binary',
+        'metric': 'auc',
+        'sub_feature': 0.7,
+        'num_leaves': 60,
+        'min_data': 100,
+        'min_hessian': 1,
+        'verbose': 2
+    }
+
+    # split data
+    train_data, valid_data = split_data(data_df)
+
+    # train model
+    model = train_model(train_data, valid_data, parameters)
+
+    # validate
+    metric = get_model_metrics(model, train_data, valid_data)
+
+    print(metric)
+
+    # Print the resulting metrics for the model
+
+
+if __name__ == '__main__':
+    main()
